b36dddfb86d97a2cd86723a4f2d4b3cec92e79a1
angelas2 <angelas2@andrew.cmu.edu>
2025-06-18 10:52:25 -0400

    changed the nonideal axes formatting

--- leastsquares_fiberized.py
+++ leastsquares_fiberized.py
@@ -12,7 +12,7 @@ def calc_expected_counts(rotations):
     # the thetas are the Euler angles of T (degrees)
     theta1, theta2, theta3 = random.randrange(0, 360), random.randrange(0, 180), random.randrange(0, 360)
     F = r.random().as_matrix()
-    T = rotation_nonideal_axes(axis1, axis2, axis3, [theta1, theta2, theta3], degrees=True)
+    T = rotation_nonideal_axes(axes[0], axes[1], axes[2], [theta1, theta2, theta3], degrees=True)
 
     # theta and phi are the spherical angles for the first row of F. (radians)
     # We also need to adjust the range of theta, since the range for arctan is [-pi/2, pi.2], but we prefer [0, 2pi]
@@ -28,7 +28,7 @@ def calc_expected_counts(rotations):
 
     actual_x = np.asarray([theta1, theta2, theta3, theta, phi, N_H, N_D])
 
-    sigma = .001       # standard deviation for noise
+    sigma = .00       # standard deviation for noise
     generated_counts = np.empty(2 * num_rotations)
     for i in range(num_rotations):
         # Map C_H to the even indices and C_D to the odd ones
@@ -49,7 +49,7 @@ def calc_expected_counts(rotations):
 def residuals(var):
     res = np.empty(2 * num_rotations)
     for index in range(num_rotations):
-        calc_T = rotation_nonideal_axes(axis1, axis2, axis3, [var[0], var[1], var[2]], degrees=True)
+        calc_T = rotation_nonideal_axes(axes[0], axes[1], axes[2], [var[0], var[1], var[2]], degrees=True)
         F_row1 = np.asmatrix(
             np.asarray([np.cos(var[3]) * np.sin(var[4]), np.sin(var[3]) * np.sin(var[4]), np.cos(var[4])]))
         calculated_C_H = 0.5 * var[5] * \
@@ -62,7 +62,7 @@ def residuals(var):
     return res
 
 
-def least_squares_fitting(data, cost_threshold=10.0):
+def least_squares_fitting(cost_threshold=10.0):
 
     success = False
     fitting = None
@@ -70,7 +70,7 @@ def least_squares_fitting(data, cost_threshold=10.0):
         x0 = [random.randrange(0, 360), random.randrange(0, 180), random.randrange(0, 360),
               random.uniform(0, 2 * np.pi), random.uniform(0, np.pi),
               max_C_H, max_C_D]
-        fitting = least_squares(residuals, x0, bounds=bounds, max_nfev=500, ftol=1e-10, verbose=1)
+        fitting = least_squares(residuals, x0, bounds=bounds, max_nfev=500, ftol=1e-10, xtol=1e-9, verbose=1)
         success = fitting.cost < cost_threshold
         # print(fitting.x)
     return fitting
@@ -78,7 +78,7 @@ def least_squares_fitting(data, cost_threshold=10.0):
 
 if __name__ == "__main__":
     num_rotations = 16
-    # rotation_list = r.random(num_rotations).as_matrix()
+    # rotation_list = r.random(num_rotations)
     rotation_list = r.from_rotvec(np.asarray([[0, 0, 0],
                                               [1, 0, 0],
                                               [2, 0, 0],
@@ -95,24 +95,23 @@ if __name__ == "__main__":
                                               [2, 0, 2],
                                               [0, 1, 1],
                                               [1, 1, 1]]))
-    axis1 = np.asarray([[.999633], [.0038151], [-.0268291]])
-    axis2 = np.asarray([[.0271085], [.999295], [.0259618]])
-    axis3 = np.asarray([[.9994289], [-.0335444], [.004005751]])
-    axis3 = np.asarray([[0], [1], [0]])
-    axis4 = np.asarray([[.997268], [-0.0702493], [0.0228234]])
-    axis5 = np.asarray([[-0.00005461419], [.999687], [-0.0250044]])
+    axes = np.asarray([[[.999633], [.0038151], [-.0268291]],
+                       [[.0271085], [.999295], [.0259618]],
+                       [[.9994289], [-.0335444], [.004005751]],
+                       [[0], [1], [0]],
+                       [[.997268], [-0.0702493], [0.0228234]],
+                       [[-0.00005461419], [.999687], [-0.0250044]]])
 
     # counts, actual_x = calc_expected_counts(rotation_list)
     # print(actual_x)
-    # print(rotation_nonideal_axes(axis1, axis2, axis3, [actual_x[0], actual_x[1], actual_x[2]], degrees=True))
+    # print(rotation_nonideal_axes(axes[0], axes[1], axes[2], [actual_x[0], actual_x[1], actual_x[2]], degrees=True))
     # print(np.asarray([np.cos(actual_x[3]) * np.sin(actual_x[4]), np.sin(actual_x[3]) * np.sin(actual_x[4]), np.cos(actual_x[4])]))
-    counts = measure(num_rotations, generate_eulerangles(rotations=rotation_list), yaml_fn='serverinfo.yaml',
-                     verbose=True, datapath='data/data.txt')
-    # counts = np.loadtxt('data/data.txt')
+    # counts = measure(num_rotations, generate_eulerangles(rotations=rotation_list), yaml_fn='serverinfo.yaml',
+    #                  verbose=True, datapath='data/data.txt')
+    counts = np.loadtxt('data/data.txt')
     # print(counts)
 
     counts_reorganized = np.reshape(counts, (2, num_rotations), order='F')
-    print(counts_reorganized)
     max_C_H = max(counts_reorganized[0])
     max_C_D = max(counts_reorganized[1])
     bounds = ([0, 0, 0, 0, 0, max_C_H, max_C_D], [360, 180, 360, 2 * np.pi, np.pi, np.inf, np.inf])
@@ -123,11 +122,11 @@ if __name__ == "__main__":
     T_matrices = np.empty((n, 9))
     F_matrices = np.empty((n, 3))
     for i in range(n):
-        result = least_squares_fitting(counts, cost_threshold=1)
+        result = least_squares_fitting(cost_threshold=1)
 
         x = result.x
         outputs[i] = x
-        calculated_T = rotation_nonideal_axes(axis1, axis2, axis3, [x[0], x[1], x[2]], degrees=True)
+        calculated_T = rotation_nonideal_axes(axes[0], axes[1], axes[2], [x[0], x[1], x[2]], degrees=True)
         T_matrices[i] = calculated_T.reshape(9)
         print("\nCalculated T: \n", calculated_T)
         f = np.asmatrix(np.asarray([np.cos(x[3]) * np.sin(x[4]), np.sin(x[3]) * np.sin(x[4]), np.cos(x[4])]))
@@ -135,7 +134,8 @@ if __name__ == "__main__":
         print("f: ", f)
         print("Result: ", x)
         print("Cost: ", result.cost)
+        print(residuals(x))
 
-    np.savetxt('data/leastsquares_output.txt', outputs)
-    np.savetxt('data/T_matrices.txt', T_matrices)
-    np.savetxt('data/F.txt', F_matrices)
+    # np.savetxt('data/leastsquares_output1.txt', outputs)
+    # np.savetxt('data/T_matrices1.txt', T_matrices)
+    # np.savetxt('data/F1.txt', F_matrices)
