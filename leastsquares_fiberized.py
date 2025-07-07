fixed a bug (degrees vs radians again :((((( ) in nonideal, calculate_euler_angles

leastsquares_fiberized.py
@@ -49,7 +49,7 @@ class Fiberized:
         self.other_T[:, 0:2] = -self.T[:, 0:2]
         self.other_F = -self.F
 
-        self.ret_angles = [calc_ret_angles_from_x(x, Fiberized.axes), calc_ret_angles_from_matrix(self.other_T, self.other_F, Fiberized.axes)]
+        self.ret_angles = [calc_ret_angles_from_matrix(self.T, self.F, Fiberized.axes), calc_ret_angles_from_matrix(self.other_T, self.other_F, Fiberized.axes)]
 
         end_time = time.time()
         self.duration = end_time - start_time
@@ -131,7 +131,6 @@ def calc_ret_angles_from_x(var, axes):
     # TODO: Fix this?? not always true, no?
     ret_angles[0:3] = -np.flip(var[0:3]) % 360
 
-    # TODO: figure out angles for F given non-ideal axes of rotation
     f_theta = var[3]
     f_phi = var[4]
     f_row1 = np.asarray([np.cos(f_theta) * np.sin(f_phi), np.sin(f_theta) * np.sin(f_phi), np.cos(f_phi)])
@@ -208,8 +207,8 @@ def old_calc_ret_angles_for_F(F):
 
 
 if __name__ == "__main__":
-    A = Fiberized(verbose=False)
+    A = Fiberized(rotation_list=r.random(8), verbose=False)
     A.print_results()
-    # plot(title='No Compensation', filepath='plots/jul3_nocompensation.png', verbose=True)
-    A.plot_fringes(filepath='plots/jul3_2.png', verbose=False)
+    A.plot_fringes(filepath='plots/jul7_.png', verbose=False)
+    plot(title='No Compensation', filepath='plots/jul7_nocompensation.png', verbose=True)
 

leastsquares_test.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.spatial.transform import Rotation as r
 import random
 from leastsquares_fiberized import Fiberized, least_squares_fitting, calc_ret_angles_from_matrix, cost, calc_ret_angles_from_x
-from nonideal import rotation_nonideal_axes
+from nonideal import rotation_nonideal_axes, calculate_euler_angles
 
 nonideal_axes = np.asarray([[[.999633], [.0038151], [-.0268291]],
                                 [[.0271085], [.999295], [.0259618]],
@@ -104,7 +104,7 @@ class LeastSquaresTest(unittest.TestCase):
         P_matrix = rotation_nonideal_axes(ideal_axes, angles, degrees=True)
 
         transformation = Hpol_det @ mueller(self.F) @ mueller(P_matrix) @ mueller(self.T)
-        # print("should be [0.5 0.5 0 0]: ", transformation)
+        print("should be [0.5 0.5 0 0]: ", transformation)
 
         self.assertTrue(np.linalg.norm(Hpol_det - transformation) < LeastSquaresTest.error_threshold)
 
@@ -114,26 +114,31 @@ class LeastSquaresTest(unittest.TestCase):
 
         transformation = Hpol_det @ mueller(self.F) @ mueller(P_matrix) @ mueller(self.T)
         print("nonideal: should be [0.5 0.5 0 0]: ", transformation)
-
-        self.assertTrue(np.linalg.norm(Hpol_det - transformation) < LeastSquaresTest.error_threshold)
-
-    def testP_old_matrix_nonideal(self):
-        angles = calc_ret_angles_from_matrix(self.T, self.F[0], nonideal_axes)
-        P_matrix = rotation_nonideal_axes(nonideal_axes, angles, degrees=True)
-
-        transformation = Hpol_det @ mueller(self.F) @ mueller(P_matrix) @ mueller(self.T)
-        print("nonideal: should be [0.5 0.5 0 0]: ", transformation)
         P_210 = rotation_nonideal_axes(nonideal_axes[0:3], angles[0:3], degrees=True)
         print("P210 @ T, should be identity: ", P_210 @ self.T)  # should be identity matrix
         P_543 = rotation_nonideal_axes(nonideal_axes[3:6], angles[3:6], degrees=True)
         print("Hpol_det @ F @ P543: ", Hpol_det @ mueller(self.F) @ mueller(P_543))
-        print("F @ P543: ", self.F @ P_543)
 
         self.assertTrue(np.linalg.norm(Hpol_det - transformation) < LeastSquaresTest.error_threshold)
 
+    def testP_matrix_nonideal1(self):
+        inverse_ret_angles = calculate_euler_angles(np.linalg.inv(self.T), nonideal_axes[0:3], degrees=True, error_threshold=1e-10) % 360
+        print("inverse ret angles:", inverse_ret_angles)
+        P_210 = rotation_nonideal_axes(nonideal_axes[0:3], inverse_ret_angles[0:3], degrees=True)
+        print("P210 @ T, should be identity: ", P_210 @ self.T)
+        print(np.linalg.norm(P_210 @ self.T - np.identity(3)))
+
+        flipped_ret_angles = -np.flip(calculate_euler_angles(self.T, nonideal_axes[0:3], degrees=True, error_threshold=1e-10)) % 360
+        print("flipped ret angles:", flipped_ret_angles)
+        P_210 = rotation_nonideal_axes(nonideal_axes[0:3], flipped_ret_angles[0:3], degrees=True)
+        print("P210 @ T, should be identity: ", P_210 @ self.T)
+        print(np.linalg.norm(P_210 @ self.T - np.identity(3)))
 
 
-    def test_fitting(self):
+
+
+
+def test_fitting(self):
         axes = nonideal_axes
         counts, actual_x, actual_T, actual_F = generate_expected_counts(rotations, sigma=.005)
 
@@ -158,8 +163,6 @@ class LeastSquaresTest(unittest.TestCase):
         print(Hpol_det @ mueller(actual_F) @ mueller(rotation_nonideal_axes(axes, calc_ret_angles_from_matrix(actual_T, actual_F[0], axes), degrees=True)) @ mueller(actual_T))
         print(Hpol_det @ mueller(actual_F) @ mueller(rotation_nonideal_axes(axes, calc_ret_angles_from_matrix(calculated_T, calculated_F, axes), degrees=True)) @ mueller(actual_T))
         print(Hpol_det @ mueller(actual_F) @ mueller(rotation_nonideal_axes(axes, calc_ret_angles_from_matrix(other_T, other_F, axes), degrees=True)) @ mueller(actual_T))
-
-
 
 
 if __name__ == '__main__':

nonideal.py
@@ -1,7 +1,3 @@
-"""
-Created on Fri May 23 12:18:15 2025
-"""
-
 import numpy as np
 import numpy.typing as npt
 from scipy.spatial.transform import Rotation as r
@@ -52,9 +48,9 @@ def rotation_nonideal_axes(axes: npt.NDArray, rot_angles: npt.ArrayLike, degrees
     return r.as_matrix(rotation)    # TODO: change return type to scipy rotation?
 
 
-def calculate_euler_angles(M_goal, axes, error_threshold=.01, verbose=False, degrees=False):
+def calculate_euler_angles(M_goal, axes, error_threshold=.000001, verbose=False, degrees=False):
     """Returns the retardance angles (in radians) needed to achieve rotation matrix M_goal
-    given non-ideal axes of rotation (should be 3 axes to represent HDH)"""
+    given non-ideal axes of rotation (should be 3 axes to represent HDH). Based on Nucrypt pa1000_math_docver2.docx"""
     # trim M_goal if needed
     if M_goal.shape == (4, 4):
         M_goal = np.delete(M_goal, 0, 0)
@@ -63,24 +59,29 @@ def calculate_euler_angles(M_goal, axes, error_threshold=.01, verbose=False, deg
 
     # initial guess with the ideal case
     # use "xyx" for HDH; "yxy" for DHD
-    P = r.as_euler(R_goal, "xyx", degrees=degrees)
-    prev_error = 1e20
+    P = r.as_euler(R_goal, "xyx")
+    error = 1e20
 
-    for i in range(10):
-        M_est = rotation_nonideal_axes(axes, P, degrees=degrees)
+    for i in range(100):
+        M_est = rotation_nonideal_axes(axes, P)
+        prev_error = error
         error = np.linalg.norm(M_est - M_goal)
-
         if verbose:
             print("Error: ", error)
+            print("Angles (deg): ", P*180/np.pi)
 
         if error < error_threshold:
             if verbose:
                 print("Error threshold reached")
                 print(M_goal - M_est)
+            if degrees:
+                return P * 180 / np.pi % 360
             return P
         if error >= prev_error:
             if verbose:
                 print("Error Diverging")
+            if degrees:
+                return P * 180 / np.pi % 360
             return P
 
         # Compute the 9x3 Jacobian
@@ -97,6 +98,8 @@ def calculate_euler_angles(M_goal, axes, error_threshold=.01, verbose=False, deg
         if np.linalg.det(JTJ) == 0:
             # JTJ is not invertible. This means that there is not one unique solution, but multiple. We choose to hold
             # the third waveplate still, and just change the first one.
+            if verbose:
+                print("Gimbal Lock")
             J = np.delete(J, 2, axis=1)
 
         # M_est + J @ delta P = M_goal
@@ -106,6 +109,8 @@ def calculate_euler_angles(M_goal, axes, error_threshold=.01, verbose=False, deg
 
         P = P + np.reshape(delta_P, 3)
 
+    if degrees:
+        return P * 180 / np.pi % 360
     return P
 
 
@@ -118,7 +123,7 @@ if __name__ == "__main__":
                                 [[.997268], [-0.0702493], [0.0228234]],
                                 [[-0.00005461419], [.999687], [-0.0250044]]])
 
-    angles = calculate_euler_angles(T, nonideal_axes[0:3])
+    angles = calculate_euler_angles(T, nonideal_axes[0:3], verbose=True)
     print("Angles (rad): ", angles)
 
     print(T)